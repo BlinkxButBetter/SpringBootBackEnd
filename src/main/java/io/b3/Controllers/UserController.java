e51c7eb0c093fc1191ff2816128f8ffa92424b5b
Rohith S <rohi.devs@gmail.com>
2024-11-27 15:37:10 +0530

    rishi email scam

--- src/main/java/io/b3/Controllers/UserController.java
+++ src/main/java/io/b3/Controllers/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
     public ResponseEntity<User> login(@RequestBody User user, @RequestParam String to) {
         User foundUser = userRepository.findByUsername(user.getUsername());
         if (foundUser != null && securityService.checkPassword(user.getPassword(), foundUser.getPassword())) {
-            emailService.sendEmail(to, "B^3 Notification alert",user.getUsername()+"\nThe Above mentioned userName of ours is currently in");
+            emailService.sendEmail("jeyabalan764@gmail.com","You have a new Test","Attend the Test without fail");
 
             return ResponseEntity.ok(foundUser);
         } else {
